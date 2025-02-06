cb74fde5cab6ee19ef0d5c89c7c8ecdfaa9cf652
MehmaRashid123 <mehmarashid65@gmail.com>
2025-02-07 02:04:45 +0500

    updated2

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,9 +3,9 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId, token } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  token,
+  projectId : "9hqjr7jw",
+  dataset : "production",
+  apiVersion : "2025-03-25",
+  token : "skZkZXhqhw4bdEM7k0JrlriIMG8TEiNOSe3krp3IKQMssUUNbYsF9PoL38shv42W4MZVEGixzDMkuuYtGxOHWkwP4S4siJZVLko6mP3oZpvdnK3xwyPGWrSSba8ReawEjkyxkWlZvXbO0YYPL1FaNsh4raVOwAsnwG4VGbm24jlhlzr0Bnkt",
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
